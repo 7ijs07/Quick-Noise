101a366d400ba59e1d5a13e4f486bf638bb51a56
Octal <oliver@olivermakesco.de>
2026-02-19 19:57:45 -0600

    use cfg_if to select SIMD sizes

--- src/simd/arch_simd.rs
+++ src/simd/arch_simd.rs
@@ -4,30 +4,24 @@ use std::marker::PhantomData;
 
 // Static dispatch for identifying lane sizes and number of simd registers.
 
-#[cfg(target_feature = "avx512f")]
-const SIMD_WIDTH: usize = 64;
-#[cfg(target_feature = "avx512f")]
-const NUM_SIMD_REG: usize = 32;
-
-#[cfg(target_feature = "avx2")]
-const SIMD_WIDTH: usize = 32;
-#[cfg(target_feature = "avx2")]
-const NUM_SIMD_REG: usize = 16;
-
-#[cfg(target_feature = "neon")]
-const SIMD_WIDTH: usize = 16;
-#[cfg(target_feature = "neon")]
-const NUM_SIMD_REG: usize = 32;
-
-#[cfg(all(target_feature = "sse2", not(target_feature = "avx2")))]
-const SIMD_WIDTH: usize = 16;
-#[cfg(all(target_feature = "sse2", not(target_feature = "avx2")))]
-const NUM_SIMD_REG: usize = 16;
-
-#[cfg(not(any(target_feature = "avx2", target_feature = "sse2")))]
-const SIMD_WIDTH: usize = 1;
-#[cfg(not(any(target_feature = "avx2", target_feature = "sse2")))]
-const NUM_SIMD_REG: usize = 8;
+cfg_if::cfg_if!{
+    if #[cfg(target_feature = "avx512f")] {
+        const SIMD_WIDTH: usize = 64;
+        const NUM_SIMD_REG: usize = 32;
+    } else if #[cfg(target_feature = "avx2")] {
+        const SIMD_WIDTH: usize = 32;
+        const NUM_SIMD_REG: usize = 16;
+    } else if #[cfg(target_feature = "sse2")] {
+        const SIMD_WIDTH: usize = 16;
+        const NUM_SIMD_REG: usize = 16;
+    } else if #[cfg(target_feature = "neon")] {
+        const SIMD_WIDTH: usize = 16;
+        const NUM_SIMD_REG: usize = 32;
+    } else {
+        const SIMD_WIDTH: usize = 1;
+        const NUM_SIMD_REG: usize = 8;
+    }
+}
 
 pub type ArchSimd<T: SimdInfo> = Simd<T, { T::LANES }>;
 
