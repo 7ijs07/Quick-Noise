9f9ed73e5fe39e24a2f41d5a44d50230753efd58
Octal <oliver@olivermakesco.de>
2026-02-20 23:31:34 -0600

    Add support for more SIMD

--- src/simd/arch_simd.rs
+++ src/simd/arch_simd.rs
@@ -4,21 +4,43 @@ use std::marker::PhantomData;
 
 // Static dispatch for identifying lane sizes and number of simd registers.
 
-cfg_if::cfg_if!{
-    if #[cfg(target_feature = "avx512f")] {
+cfg_if::cfg_if! {
+    // x86_64
+    if #[cfg(all(target_arch = "x86_64", target_feature = "avx512f"))] {
         const SIMD_WIDTH: usize = 64;
         const NUM_SIMD_REG: usize = 32;
-    } else if #[cfg(target_feature = "avx2")] {
+    } else if #[cfg(all(target_arch = "x86_64", target_feature = "avx2"))] {
         const SIMD_WIDTH: usize = 32;
         const NUM_SIMD_REG: usize = 16;
-    } else if #[cfg(target_feature = "sse2")] {
+    } else if #[cfg(all(target_arch = "x86_64", target_feature = "sse2"))] {
         const SIMD_WIDTH: usize = 16;
         const NUM_SIMD_REG: usize = 16;
-    } else if #[cfg(target_feature = "neon")] {
+    }
+
+    // aarch64
+    else if #[cfg(all(target_arch = "aarch64", target_feature = "sve"))] {
+        const SIMD_WIDTH: usize = 32;
+        const NUM_SIMD_REG: usize = 32;
+    } else if #[cfg(all(target_arch = "aarch64", target_feature = "neon"))] {
         const SIMD_WIDTH: usize = 16;
         const NUM_SIMD_REG: usize = 32;
-    } else {
-        const SIMD_WIDTH: usize = 1;
+    }
+
+    // wasm
+    else if #[cfg(all(any(target_arch = "wasm32", target_arch = "wasm64"), target_feature = "simd128"))] {
+        const SIMD_WIDTH: usize = 16;
+        const NUM_SIMD_REG: usize = 16;
+    }
+
+    // riscv
+    else if #[cfg(all(any(target_arch = "riscv64", target_arch = "riscv32"), target_feature = "v"))] {
+        const SIMD_WIDTH: usize = 32;
+        const NUM_SIMD_REG: usize = 32;
+    }
+
+    // fallback
+    else {
+        const SIMD_WIDTH: usize = 4;
         const NUM_SIMD_REG: usize = 8;
     }
 }
